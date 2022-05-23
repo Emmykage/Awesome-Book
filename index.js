8858572ccae26e4f08b4486affb757a4bb13bb03
jevazquezb <je.vazquezb@gmail.com>
2022-05-23 16:49:49 -0500

    Add a function to add each book to localStorage and display it in the browser

--- index.js
+++ index.js
@@ -3,42 +3,53 @@ const addBtn = document.getElementById("add");
 const body = document.querySelector(".container");
 const bookName = document.getElementById("name");
 const bookAuthor = document.getElementById("author");
+const bookList = [];
 
+function displayBook (title, author){
+    let bookContainer = document.querySelector('.col-1');
 
-
-
-function addBook (){
     let bkdiv = document.createElement("div");
+    // Title of the book
     let addTitle = document.createElement("h2");
-    let addAuthor = document.createElement("p");
-    let rmBtn = document.createElement("button");
-    let hr = document.createElement("hr");
-
-
-    rmBtn.innerHTML = "Remove";
-
-    body.appendChild(bkdiv);
+    addTitle.textContent = title;
     bkdiv.appendChild(addTitle);
+
+    //Author of the book
+    let addAuthor = document.createElement("p");
+    addAuthor.textContent = author;
     bkdiv.appendChild(addAuthor);
+
+    //Remove Button
+    let rmBtn = document.createElement("button");
+    rmBtn.textContent = "Remove";
     bkdiv.appendChild(rmBtn);
+
+    let hr = document.createElement("hr");
     bkdiv.appendChild(hr);
 
-//     let book = {title: 'bookName.value', author: 'bookAuthor.value'};
-// bookList.push(book);
+    bookContainer.appendChild(bkdiv);
 
-
-    
-    // let books = new Object;
+      // let books = new Object;
     // // books.title =  bkname.value;
     // books.author =  bkAuthor.value;
     
 
-    addTitle.innerHTML = bookName.value;
-    addAuthor.innerHTML = bookAuthor.value;
-
-    
-    
-
+    //addTitle.innerHTML = bookName.value;
+    //addAuthor.innerHTML = bookAuthor.value;
 };
 
-addBtn.addEventListener("click", addBook);
+function addBook(title, author) {
+    let book = {title, author};
+    bookList.push(book);
+    console.log(bookList);
+}
+
+function saveBook() {
+    localStorage.setItem('bookList', JSON.stringify(bookList));
+}
+
+addBtn.addEventListener("click", () => {
+    addBook(bookName.value, bookAuthor.value);
+    saveBook();
+    displayBook(bookName.value, bookAuthor.value);   
+});
