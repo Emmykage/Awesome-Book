f05f0d3da16a483627a067595718106776920585
Menanya <emmmiemenz@gmail.com>
2022-05-23 21:11:09 +0100

    second commit

--- index.js
+++ index.js
@@ -3,9 +3,6 @@ const booklist = [{
     author : "",
 }]
 
-const addBtn = document.getElementById("add");
-
-const body = document.querySelector(".container");
 
 let books = new Object();
 books.title = "to kill a mocking bird";
@@ -13,10 +10,34 @@ books.author = "Philip Lahm";
 
 console.log(books);
 
+const addBtn = document.getElementById("add");
+
+const body = document.querySelector(".container");
+const bkName = document.getElementById("name");
+const bkAuthor = document.getElementById("author");
+
+
 
 
 function addBook (){
     let bkdiv = document.createElement("div");
+    let addTitle = document.createElement("h2");
+    let addAuthor = document.createElement("p");
+    let rmBtn = document.createElement("button");
+    let hr = document.createElement("hr");
+
+    body.appendChild(bkdiv);
+    bkdiv.appendChild(addTitle);
+    bkdiv.appendChild(addAuthor);
+    bkdiv.appendChild(hr);
+
+
+
+    books.title =  bkname.value;
+    books.author =  bkAuthor.value;
+
+    addTitle.innerHTML = books.title
+    
     
 
 }
---
+++ script.js
@@ -0,0 +1,9 @@
+let cat = {
+
+}
+let dog = {
+    
+}
+let Thecat = new Object;
+Thecat.alsatian = "tom";
+console.log(Thecat);
