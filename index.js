867030bc98ed95fa1930feb22d1f6f6321aa88f5
Menanya <emmmiemenz@gmail.com>
2022-05-26 15:34:56 +0100

    add input valueto page

--- index.js
+++ index.js
@@ -1,300 +1,52 @@
-// /* eslint-disable max-classes-per-file */
+const addBtn = document.getElementById("add");
+const mainCol = document.querySelector(".col-1");
 
-// // Extracted function from lesson
-// function storageAvailable(type) {
-//   let storage;
-//   try {
-//     const x = '__storage_test__';
-//     storage = window[type];
-//     storage.setItem(x, x);
-//     storage.removeItem(x);
-//     return true;
-//   } catch (e) {
-//     return (
-//       e instanceof DOMException
-//                 // everything except Firefox
-//                 && (e.code === 22
-//                 // Firefox
-//                 || e.code === 1014
-//                 // test name field too, because code might not be present
-//                 // everything except Firefox
-//                 || e.name === 'QuotaExceededError'
-//                 // Firefox
-//                 || e.name === 'NS_ERROR_DOM_QUOTA_REACHED')
-//                 // acknowledge QuotaExceededError only if there's something already stored
-//                 && storage
-//                 && storage.length !== 0
-//     );
-//   }
-// }
-
-// const bookName = document.getElementById('name');
-// const bookAuthor = document.getElementById('author');
-// const addBtn = document.getElementById('add');
-// let books;
-
-// class Book {
-//   constructor(title = null, author = null, id = null) {
-//     this.title = title;
-//     this.author = author;
-//     this.id = id;
-//   }
-// }
-
-// class BookManager {
-//   constructor() {
-//     this.bookList = null;
-//   }
-
-//   getBooks() {
-//     this.bookList = JSON.parse(localStorage.getItem('bookList')) || [];
-//     return this.bookList;
-//   }
-
-//   saveBooks() {
-//     localStorage.setItem('bookList', JSON.stringify(this.bookList));
-//   }
-
-//   addBook(title, author) {
-//     const bookId = Math.random().toString(36).replace(/[^a-z]+/g, '').slice(2, 5);
-//     const newBook = new Book(title, author, bookId);
-//     this.bookList.push(newBook);
-//     return bookId;
-//   }
-
-//   removeBook(e) {
-//     // Remove from localStorage
-//     this.bookList = JSON.parse(localStorage.getItem('bookList'));
-//     const bookId = e.target.id;
-//     const filteredBooks = this.bookList.filter((book) => book.id !== bookId);
-//     localStorage.setItem('bookList', JSON.stringify(filteredBooks));
-
-//     // Remove from the Interface (DOM)
-//     e.target.parentElement.parentElement.remove();
-//   }
-// }
-
-// const library = new BookManager();
-
-// function displayBook(title, author, id) {
-//   // Book list container
-//   const bookContainer = document.querySelector('.col-1');
-
-//   // Book container
-//   const bkdiv = document.createElement('div');
-//   bkdiv.classList.add('flex');
-
-//   // book-details container
-//   const bkdetails = document.createElement('div');
-//   bkdetails.classList.add('bk-details');
-//   bkdiv.appendChild(bkdetails);
-
-//   // Title of the book
-//   const addTitle = document.createElement('h2');
-//   addTitle.textContent = `"${title}"`;
-//   bkdetails.appendChild(addTitle);
-
-//   // paragraph insertion
-
-//   const paragraph = document.createElement('p');
-//   bkdetails.appendChild(paragraph);
-//   paragraph.innerText = 'by';
-
-//   // Author of the book
-//   const addAuthor = document.createElement('p');
-//   addAuthor.innerHTML = author;
-//   bkdetails.appendChild(addAuthor);
-
-//   // btn-details container
-//   const btnDiv = document.createElement('div');
-//   btnDiv.classList.add('btn-details');
-//   bkdiv.appendChild(btnDiv);
-
-//   // Remove Button
-//   const rmBtn = document.createElement('button');
-//   rmBtn.textContent = 'Remove';
-//   rmBtn.classList.add('rmbtn');
-//   rmBtn.id = id;
-//   rmBtn.addEventListener('click', library.removeBook);
-//   btnDiv.appendChild(rmBtn);
-
-//   // Parting line
-//   // const hr = document.createElement('hr');
-//   // bkdiv.appendChild(hr);
-
-//   bookContainer.appendChild(bkdiv);
-// }
-
-// function resetInput() {
-//   bookName.value = '';
-//   bookAuthor.value = '';
-// }
-
-// if (storageAvailable('localStorage')) {
-//   window.addEventListener('load', () => {
-//     books = library.getBooks();
-//     if (books.length !== 0) {
-//       books.forEach((book) => displayBook(book.title, book.author, book.id));
-//     }
-//   });
-//   addBtn.addEventListener('click', () => {
-//     library.getBooks();
-//     const bookId = library.addBook(bookName.value, bookAuthor.value);
-//     library.saveBooks();
-//     displayBook(bookName.value, bookAuthor.value, bookId);
-//     resetInput();
-//   });
-// }
-
-
-class Books{
+class Books {
   constructor(title, author){
     this.title = title;
-    this.author = author
+    this.author = author;
   }
 }
-const bookLibrary = [{
-  title: "Arabian Knight",
-  author: "Aladin"
-},
-{
-  title: "King Kuta",
-  author: "Ahmed"
-}];
+class UI {
+  static addBooks(title, author){
+    const bookRow =  document.createElement("div");
+    bookRow.classList.add("flex");
 
-class Interface{
-  static display(){
-    bookLibrary.forEach(book => Interface.addBook(book))
+    const bookDetails = document.createElement("div");
+    bookDetails.classList.add("bk-details");
 
-  
-  }
-  static deleteBook(target){
+    const bookTitle = document.createElement("h2");
+    bookTitle.innerHTML = title;
+    bookDetails.appendChild(bookTitle)
 
-    if(target.classList.contains("delBtn"))
-    target.parentNode.parentNode.remove();
+    const byP = document.createElement("p");
+    byP.innerText = "By";
+
+    bookDetails.appendChild(byP);
+
+    const bookAuthor = document.createElement("p");
+    bookAuthor.innerText = author;
+    bookDetails.appendChild(bookAuthor);
+
+    bookRow.appendChild(bookDetails);
+    mainCol.appendChild(bookRow);
     
 
-  }
-  static clearField(){
-const title = document.getElementById("title");
-const author = document.getElementById("author");
-
-author.value = "";
-author.value= ""
+    console.log(title, author)
 
   }
-
-  static showAlert(message, classname){
-    const messdiv = document.createElement("div");
-    messdiv.className = `alert alert-${classname}`;
-    messdiv.innerHTML = message;
-    document.querySelector(".col-1").prepend(messdiv);
-
-    setTimeout(() => {
-      messdiv.remove();
-      
-    }, 2000);
-  }
-
-  static addBook(book){
-
-
-    // Book list container
-      const bookContainer = document.querySelector('.col-1');
-    
-      // Book container
-      const bkdiv = document.createElement('div');
-      bkdiv.classList.add('flex');
-    // // -------------------- come back to reconcile this------------------
-    //   // book-details container
-    //   const bkdetails = document.createElement('div');
-    //   bkdetails.classList.add('bk-details');
-    //   bkdiv.appendChild(bkdetails);
-    
-    //   // Title of the book
-    //   const addTitle = document.createElement('h2');
-    //   addTitle.innerHTML = `"${x}"`;
-    //   bkdetails.appendChild(addTitle);
-    
-    //   // paragraph insertion
-    
-    //   const paragraph = document.createElement('p');
-    //   bkdetails.appendChild(paragraph);
-    //   paragraph.innerText = 'by';
-    
-    //   // Author of the book
-    //   const addAuthor = document.createElement('p');
-    //   addAuthor.innerHTML = y;
-    //   bkdetails.appendChild(addAuthor);
-    
-    //   // btn-details container
-    //   const btnDiv = document.createElement('div');
-    //   btnDiv.classList.add('btn-details');
-    //   bkdiv.appendChild(btnDiv);
-    
-    //   // Remove Button
-
-
-    //   const rmBtn = document.createElement('button');
-    //   rmBtn.textContent = 'Remove';
-    //   rmBtn.classList.add('rmbtn');
-
-
-    //   // rmBtn.id = id;
-    //   // rmBtn.addEventListener('click', library.removeBook);
-    //   btnDiv.appendChild(rmBtn);
-
-
-      // end
-
-
-
-      // --------- second method -----------------------
-      bkdiv.innerHTML = `
-        <div class="bk-details"> <h2>"${book.title}"</h2></div>
-        <p>by</p>
-        <p> ${book.author}</p>
-
-        <div>
-        <button class="delBtn"> Remove </button>
-
-        </div>
-      
-      `
-      // ------------end-------------------
-
-      bookContainer.appendChild(bkdiv);
-
-  }
-
 }
-Interface.display()
-
-addBtn = document.getElementById("add");
-document.getElementById("form").addEventListener("submit", function(){
-
-  const title = document.getElementById("name").value;
-  const author = document.getElementById("author").value;
-
-  if(!title || !author){
-    showAlert("please enter a valid input", "danger")
-    return
-  }
 
 
-  const book = new Books(title, author);
-  Interface.addBook(book);
+addBtn.addEventListener("click", addLibrary);
 
-  // Interface.display(title, author)
+function addLibrary(){
+  const title = document.getElementById("name");
+  const author = document.getElementById("author");
 
-  Interface.clearField()
-  Interface.showAlert("thanks", "success");
+  const book = new Books(title.value, author.value);
 
-})
+  UI.addBooks(book);
 
-document.querySelector(".col-1").addEventListener("click", removeBook);
-function removeBook(e){
- Interface.deleteBook(e.target);
- Interface.showAlert("book removed", "success");
 
 }
